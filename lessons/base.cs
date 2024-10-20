8faacc14db94f64f4533d35a9cbdb719cd2b1395
Ana-stasiya <nastya.vorono@gmail.com>
2024-10-20 13:37:08 +0300

    Добавьте файлы проекта.

---
+++ lessons/base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Building
+{
+    public int floor;
+    public int Area;
+    public int Occupants;
+
+    public int AreaPerPerson()
+    {
+        return Area / Occupants;
+    }
+}
